f4e3cda02c8a8f618d11544ae2c4d4b3541047f0
Demur Rumed <junkmute@hotmail.com>
2010-12-07 04:09:33 +0000

    Tightened the code. Performance found in removing newlines (Removes 2 NOPs per loop if running without -O), pruning g2/g3 from cipher, cache sbox/i_sbox

--- aespython/aes_cipher.py
+++ aespython/aes_cipher.py
@@ -18,45 +18,39 @@ __author__ = "Adam Newman"
 #Normally use relative import. In test mode use local import.
 try:from .aes_tables import sbox,i_sbox,galI,galNI
 except ValueError:from aes_tables import sbox,i_sbox,galI,galNI
-ups="(s0,s1,s2,s3,s4,s5,s6,s7,s8,s9,sa,sb,sc,sd,se,sf)"
+ups=",".join("s%x"%x for x in range(16))
 upr=ups.replace("s","r")
-mix="(%s)"%",".join(("g0[s%x]^g1[s%x]^g2[s%x]^g3[s%x]^r%x,g3[s%x]^g0[s%x]^g1[s%x]^g2[s%x]^r%x,g2[s%x]^g3[s%x]^g0[s%x]^g1[s%x]^r%x,g1[s%x]^g2[s%x]^g3[s%x]^g0[s%x]^r%x"%(i0,i1,i2,i3,i0,i0,i1,i2,i3,i1,i0,i1,i2,i3,i2,i0,i1,i2,i3,i3) for (i0,i1,i2,i3) in ((0,1,2,3),(4,5,6,7),(8,9,10,11),(12,13,14,15))))
-imix="(%s)"%",".join(("g0[s%x]^g1[s%x]^g2[s%x]^g3[s%x],g3[s%x]^g0[s%x]^g1[s%x]^g2[s%x],g2[s%x]^g3[s%x]^g0[s%x]^g1[s%x],g1[s%x]^g2[s%x]^g3[s%x]^g0[s%x]"%(i*4) for i in ((0,1,2,3),(4,5,6,7),(8,9,10,11),(12,13,14,15))))
-csl="s0 s5 sa sf s4 s9 se s3 s8 sd s2 s7 sc s1 s6 sb".split()
-csr="s0 sd sa s7 s4 s1 se sb s8 s5 s2 sf sc s9 s6 s3".split()
-box=",".join("sbox[%s]"%i for i in csl)
-ibox=",".join("i_sbox[%s]^%s"%i for i in zip(csr,upr[1:-1].split(",")))
-xor=",".join("sbox[%s]^r%x"%i for i in zip(csl,range(16)))
-ixor=",".join("i_sbox[%s]^r%x"%i for i in zip(csr,range(16)))
+mix=",".join(",".join(("g{0}[s%x]^g{1}[s%x]^g{2}[s%x]^g{3}[s%x]^r%x"%(i+(i[0]+(0,3,2,1)[j],))).format(j&3,j+1&3,j+2&3,j+3&3) for j in (0,3,2,1)) for i in ((0,1,2,3),(4,5,6,7),(8,9,10,11),(12,13,14,15))).replace("g2","").replace("g3","")
+for c in "^[",",[":
+	i=mix.find(c)
+	while i!=-1:
+		mix=mix[:i+1]+mix[i+2:i+4]+mix[i+5:]
+		i=mix.find(c,i)
+imix=",".join(",".join(("g{0}[s%x]^g{1}[s%x]^g{2}[s%x]^g{3}[s%x]"%i).format(j&3,j+1&3,j+2&3,j+3&3) for j in (0,3,2,1)) for i in ((0,1,2,3),(4,5,6,7),(8,9,10,11),(12,13,14,15)))
+csl=["s%x"%(x*5&15) for x in range(16)]
+csr=["s%x"%(x*-3&15) for x in range(16)]
+box=",".join("sx[%s]"%i for i in csl)
+ibox=",".join("ix[%s]^r%x"%i for i in zip(csr,range(16)))
+xor=",".join("sx[%s]^r%x"%i for i in zip(csl,range(16)))
 xori=";".join("s%x^=r%x"%(i,i) for i in range(16))
 ciph="""def decipher_block(f,s):
-    sek=f._expanded_key
-    g0,g1,g2,g3=galNI
-    %(ups)=s+[0]*(16-len(s))
-    %(upr)=k0
-    %(xori)
-    for i in range(!16):
-        %(upr)=sek[i:i+16]
-        %(ups)=%(box)
-        %(ups)=%(mix)
-    %(upr)=k2
-    return %(xor)""".replace("%(ups)",ups).replace("%(xori)",xori).replace("%(upr)",upr)
+ sx=sbox;ix=i_sbox;g0,g1,g2,g3=galNI;sek=f._expanded_key;S=s+[0]*(16-len(s));R=sek[:16];XI
+ for i in range(!16):R=sek[i:i+16];S=BX;S=MX
+ R=sek[f._Nr:]
+ return """.replace("S",ups).replace("XI",xori).replace("R",upr)
 class AESCipher:
-    def __init__(self, expanded_key):
-        self._expanded_key = expanded_key
-        self._Nr = len(expanded_key)-16
-    exec(ciph.replace("dec","c").replace("k0","sek[:16]").replace("!","16,f._Nr,").replace("k2","sek[f._Nr:]").replace("%(xor)",xor).replace("%(box)",box).replace("%(mix)",mix))
-    exec(ciph.replace("NI","I").replace("k0","sek[f._Nr:]").replace("!","f._Nr-16,0,-").replace("k2","sek[:16]").replace("%(xor)",ixor).replace("%(box)",ibox).replace("%(mix)",imix))
-
+    def __init__(self,expanded_key):
+        self._expanded_key=expanded_key
+        self._Nr=len(expanded_key)-16
+    exec(ciph.replace("dec","c").replace("!","16,f._Nr,").replace("BX",box).replace("MX",mix).replace("g2,g3","")+xor)
+    exec(ciph.replace("NI","I").replace(":16","f._Nr :").replace("f._Nr:",":16").replace("!","f._Nr-16,0,-").replace("BX",ibox).replace("MX",imix)+ibox)
 import unittest
 class TestCipher(unittest.TestCase):
     def test_cipher(self):
         """Test AES cipher with all key lengths"""
         import test_keys
         import key_expander
-
         test_data = test_keys.TestKeys()
-
         for key_size in 128, 192, 256:
             test_key_expander = key_expander.KeyExpander(key_size)
             test_expanded_key = test_key_expander.expand(test_data.test_key[key_size])
@@ -64,10 +58,8 @@ class TestCipher(unittest.TestCase):
             test_result_ciphertext = test_cipher.cipher_block(test_data.test_block_plaintext)
             self.assertEquals(len([i for i, j in zip(test_result_ciphertext, test_data.test_block_ciphertext_validated[key_size]) if i == j]),
                 16,msg='Test %d bit cipher'%key_size)
-
             test_result_plaintext = test_cipher.decipher_block(test_data.test_block_ciphertext_validated[key_size])
             self.assertEquals(len([i for i, j in zip(test_result_plaintext, test_data.test_block_plaintext) if i == j]),
                 16,msg='Test %d bit decipher'%key_size)
-
 if __name__ == "__main__":
     unittest.main()
