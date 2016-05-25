fadca5cb5b79cdf8363ffbfc0c2d2dc60b089e5f
Demur Rumed <gunkmute@gmail.com>
2016-05-25 01:38:08 +0000

    Collect tests together, fix tests, have decipher/cipher always return list, add .travis.yml

--- aespython/aes_cipher.py
+++ aespython/aes_cipher.py
@@ -14,17 +14,17 @@ Copyright (c) 2010, Adam Newman http://www.caller9.com/
 Licensed under the MIT license http://www.opensource.org/licenses/mit-license.php
 """
 __author__ = "Adam Newman"
+__all__ = "AESCipher",
 
 #Normally use relative import. In test mode use local import.
-try:from .aes_tables import sbox,i_sbox,galI,galNI
-except ValueError:from aes_tables import sbox,i_sbox,galI,galNI
+from .aes_tables import sbox,i_sbox,galI,galNI
 ups=",".join("s%x"%x for x in range(16))
 upr=ups.replace("s","r")
 mix=",".join(",".join(("g{0}[s%x]^g{1}[s%x]^g{2}[s%x]^g{3}[s%x]^r%x"%(i+(i[0]+(0,3,2,1)[j],))).format(j&3,j+1&3,j+2&3,j+3&3) for j in (0,3,2,1)) for i in ((0,1,2,3),(4,5,6,7),(8,9,10,11),(12,13,14,15))).replace("g2","g").replace("g3","g")
 i=mix.find("g[")
 while i!=-1:
-	mix=mix[:i]+mix[i+2:i+4]+mix[i+5:]
-	i=mix.find("g[",i)
+    mix=mix[:i]+mix[i+2:i+4]+mix[i+5:]
+    i=mix.find("g[",i)
 imix=",".join(",".join(("g{0}[s%x]^g{1}[s%x]^g{2}[s%x]^g{3}[s%x]"%i).format(j&3,j+1&3,j+2&3,j+3&3) for j in (0,3,2,1)) for i in ((0,1,2,3),(4,5,6,7),(8,9,10,11),(12,13,14,15)))
 csl=["s%x"%(x*5&15) for x in range(16)]
 csr=["s%x"%(x*-3&15) for x in range(16)]
@@ -41,24 +41,5 @@ class AESCipher:
     def __init__(self,expanded_key):
         self._expanded_key=expanded_key
         self._Nr=len(expanded_key)-16
-    exec(ciph.replace("g2,g3","").replace("dec","c").replace("!","16,f._Nr,").replace("B",box).replace("M",mix)+xor)
-    exec(ciph.replace("NI","I").replace(":16","f._Nr:").replace("f+16:",":16").replace("!","f._Nr-16,0,-").replace("sbox","i_sbox").replace("B",ibox).replace("M",imix)+ibox)
-import unittest
-class TestCipher(unittest.TestCase):
-    def test_cipher(self):
-        """Test AES cipher with all key lengths"""
-        import test_keys
-        import key_expander
-        test_data = test_keys.TestKeys()
-        for key_size in 128, 192, 256:
-            test_key_expander = key_expander.KeyExpander(key_size)
-            test_expanded_key = test_key_expander.expand(test_data.test_key[key_size])
-            test_cipher = AESCipher(test_expanded_key)
-            test_result_ciphertext = test_cipher.cipher_block(test_data.test_block_plaintext)
-            self.assertEquals(len([i for i, j in zip(test_result_ciphertext, test_data.test_block_ciphertext_validated[key_size]) if i == j]),
-                16,msg='Test %d bit cipher'%key_size)
-            test_result_plaintext = test_cipher.decipher_block(test_data.test_block_ciphertext_validated[key_size])
-            self.assertEquals(len([i for i, j in zip(test_result_plaintext, test_data.test_block_plaintext) if i == j]),
-                16,msg='Test %d bit decipher'%key_size)
-if __name__ == "__main__":
-    unittest.main()
+    exec(ciph.replace("g2,g3","").replace("dec","c").replace("!","16,f._Nr,").replace("B",box).replace("M",mix)+"["+xor+"]")
+    exec(ciph.replace("NI","I").replace(":16","f._Nr:").replace("f+16:",":16").replace("!","f._Nr-16,0,-").replace("sbox","i_sbox").replace("B",ibox).replace("M",imix)+"["+ibox+"]")
--- aespython/key_expander.py
+++ aespython/key_expander.py
@@ -13,18 +13,15 @@ Copyright (c) 2010, Adam Newman http://www.caller9.com/
 Licensed under the MIT license http://www.opensource.org/licenses/mit-license.php
 """
 __author__ = "Adam Newman"
+__all__ = "KeyExpander",
 
-#Normally use relative import. In test mode use local import.
-try:from .aes_tables import sbox,rcon
-except ValueError:from aes_tables import sbox,rcon
-from operator import xor
 class KeyExpander:
     """Perform AES Key Expansion"""
 
     _expanded_key_length = {128 : 176, 192 : 208, 256 : 240}
+    __slots__ = "_n", "_b"
 
     def __init__(self, key_length):
-        self._key_length = key_length
         self._n = key_length>>3
 
         if key_length in self._expanded_key_length:
@@ -38,6 +35,8 @@ class KeyExpander:
 
             http://en.wikipedia.org/wiki/Rijndael_key_schedule#Key_schedule_description
         """
+        from aespython.aes_tables import sbox,rcon
+        from operator import xor
         #First n bytes are copied from key
         len_new_key = len(new_key)
         if len_new_key != self._n:
@@ -54,44 +53,28 @@ class KeyExpander:
             keyarr=[sbox[i] for i in new_key[-3:]+new_key[-4:-3]]
             #First byte of output array is XORed with rcon(iter)
             keyarr[0] ^= rcon[rcon_iter]
-            nex(map(xor,keyarr, new_key[-self._n:4-self._n]))
+            nex(map(xor, keyarr, new_key[-self._n:4-self._n]))
             rcon_iter += 1
             len_new_key += 4
 
             #Run three passes of 4 byte expansion using copy of 4 byte tail of extended key
             #which is then xor'd with 4 bytes n bytes from end of extended key
             for j in 0,1,2:
-                nex(map(xor,new_key[-4:], new_key[-self._n:4-self._n]))
+                nex(map(xor, new_key[-4:], new_key[-self._n:4-self._n]))
                 len_new_key += 4
             if len_new_key >= self._b:return new_key
             else:
                 #If key length is 256 and key is not complete, add 4 bytes tail of extended key
                 #run through sbox before xor with 4 bytes n bytes from end of extended key
-                if self._key_length == 256:
-                    nex(map(xor,[sbox[x] for x in new_key[-4:]], new_key[-self._n:4-self._n]))
+                if self._n == 32:
+                    nex(map(xor, (sbox[x] for x in new_key[-4:]), new_key[-self._n:4-self._n]))
                     len_new_key += 4
                     if len_new_key >= self._b:return new_key
 
                 #If key length is 192 or 256 and key is not complete, run 2 or 3 passes respectively
                 #of 4 byte tail of extended key xor with 4 bytes n bytes from end of extended key
-                if self._key_length != 128:
-                    for j in ((0,1) if self._key_length == 192 else (0,1,2)):
-                        nex(map(xor,new_key[-4:], new_key[-self._n:4-self._n]))
+                if self._n != 16:
+                    for j in ((0,1) if self._n == 24 else (0,1,2)):
+                        nex(map(xor, new_key[-4:], new_key[-self._n:4-self._n]))
                         len_new_key += 4
                     if len_new_key >= self._b:return new_key
-
-import unittest
-class TestKeyExpander(unittest.TestCase):
-    def test_keys(self):
-        """Test All Key Expansions"""
-        import test_keys
-        test_data = test_keys.TestKeys()
-        for key_size in 128, 192, 256:
-            test_expander = KeyExpander(key_size)
-            test_expanded_key = test_expander.expand(test_data.test_key[key_size])
-            self.assertEqual (len([i for i, j in zip(test_expanded_key, test_data.test_expanded_key_validated[key_size]) if i == j]),
-                len(test_data.test_expanded_key_validated[key_size]),
-                msg='Key expansion ' + str(key_size) + ' bit')
-
-if __name__ == "__main__":
-    unittest.main()
