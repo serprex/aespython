b8b97a3d1acd9b0f61d2176a8a6edc2c2ecddaa2
Thibault Marquand <titotix@free.fr>
2016-05-29 01:08:27 +0200

    introduce setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "pythonaes",
+    version = "1.0",
+    author = "Demur Rumed",
+    url = "https://github.com/serprex/pythonaes",
+    packages = find_packages(),
+)
