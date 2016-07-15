3d80e8c1680723d14bf2928a45066fbf482441e6
Demur Rumed <gunkmute@gmail.com>
2016-07-15 03:20:42 +0000

    Support being subpackaged

--- aespython/__init__.py
+++ aespython/__init__.py
@@ -1,6 +1,15 @@
-from aespython.aes_cipher import AESCipher
-from aespython.key_expander import KeyExpander
-from aespython.mode import Mode
-from aespython.cbc_mode import CBCMode
-from aespython.cfb_mode import CFBMode
-from aespython.ofb_mode import OFBMode
+try:
+    from .aes_cipher import AESCipher
+    from .key_expander import KeyExpander
+    from .mode import Mode
+    from .cbc_mode import CBCMode
+    from .cfb_mode import CFBMode
+    from .ofb_mode import OFBMode
+except ImportError:
+    from aespython.aes_cipher import AESCipher
+    from aespython.key_expander import KeyExpander
+    from aespython.mode import Mode
+    from aespython.cbc_mode import CBCMode
+    from aespython.cfb_mode import CFBMode
+    from aespython.ofb_mode import OFBMode
+
