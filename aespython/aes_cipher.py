5e5c71cd3b5265f8ad2cce0c349e5404a489c2e4
Demur Rumed <gunkmute@gmail.com>
2016-07-16 17:43:24 +0000

    Remove slicing from aes_cipher

--- aespython/aes_cipher.py
+++ aespython/aes_cipher.py
@@ -22,21 +22,25 @@ while i!=-1:
     mix=mix[:i]+mix[i+2:i+4]+mix[i+5:]
     i=mix.find("g[",i)
 imix=",".join(",".join(("g{0}[s%x]^g{1}[s%x]^g{2}[s%x]^g{3}[s%x]"%i).format(j&3,j+1&3,j+2&3,j+3&3) for j in (0,3,2,1)) for i in ((0,1,2,3),(4,5,6,7),(8,9,10,11),(12,13,14,15)))
-csl=["s%x"%(x*5&15) for x in range(16)]
-csr=["s%x"%(x*-3&15) for x in range(16)]
+csl=["s%x"%(x&15) for x in range(0,80,5)]
+csr=["s%x"%(x&15) for x in range(0,-48,-3)]
 box=",".join("s[%s]"%i for i in csl)
 ibox=",".join("s[%s]^r%x"%i for i in zip(csr,range(16)))
 xor=",".join("s[%s]^r%x"%i for i in zip(csl,range(16)))
 xori=";".join("s%x^=r%x"%(i,i) for i in range(16))
-ciph="""def decipher_block(f,s):
- g0,g1,g2,g3=galNI;ek=f._expanded_key;S=s;s=sbox;R=ek[:16];X
- for f in range(!16):R=ek[f:f+16];S=B;S=M
- R=ek[f+16:]
+ciph="""def decipher_block(z,s):
+ g0,g1,g2,g3=galNI;ek=z._expanded_key;S=s;s=sbox;R=z._f16;X
+ for f in z._Nr:R=ek[f];S=B;S=M
+ R=z._l16
  return """.replace("S",ups).replace("R",upr).replace("X",xori)
 class AESCipher:
-    __slots__ = "_expanded_key", "_Nr"
+    __slots__ = "_expanded_key", "_Nr", "_Nrr", "_f16","_l16"
     def __init__(self,expanded_key):
-        self._expanded_key=expanded_key
-        self._Nr=len(expanded_key)-16
-    exec(ciph.replace("g2,g3","").replace("dec","c").replace("!","16,f._Nr,").replace("B",box).replace("M",mix)+xor)
-    exec(ciph.replace("NI","I").replace(":16","f._Nr:").replace("f+16:",":16").replace("!","f._Nr-16,0,-").replace("sbox","i_sbox").replace("B",ibox).replace("M",imix)+ibox)
+        nr=len(expanded_key)
+        self._expanded_key=[expanded_key[i:i+16] for i in range(0,nr,16)]
+        self._Nr=range(1,nr-16>>4,1)
+        self._Nrr=range(nr-32>>4,0,-1)
+        self._f16=self._expanded_key[0]
+        self._l16=self._expanded_key[-1]
+    exec(ciph.replace("g2,g3","").replace("dec","c").replace("B",box).replace("M",mix)+xor)
+    exec(ciph.replace("NI","I").replace("l16\n","f16\n").replace("f16;","l16;").replace("Nr","Nrr").replace("sbox","i_sbox").replace("B",ibox).replace("M",imix)+ibox)
